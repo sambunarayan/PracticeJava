a29d0a349cb1a93eb28a969ebc49b44464741dd1
sambunarayan <soyou30257@gmail.com>
2020-09-15 21:28:50 +0900

    20200915 java9 API
    ・CompletableFuture
    ・Reactive(Publisher, Subscriber)

---
+++ PracticeJava/src/test/java9/flows/SecondSubscriber.java
@@ -0,0 +1,32 @@
+package test.java9.flows;
+
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class SecondSubscriber<T> implements Subscriber<T> {
+	
+	private Subscription subscription;
+
+	@Override
+	public void onSubscribe(Subscription subscription) {
+		this.subscription = subscription;
+		subscription.request(10);
+	}
+
+	@Override
+	public void onNext(T item) {
+		System.out.println("Second Got : " + item);
+		subscription.request(1);
+	}
+
+	@Override
+	public void onError(Throwable throwable) {
+		throwable.printStackTrace();
+	}
+
+	@Override
+	public void onComplete() {
+		System.out.println("Done");
+	}
+
+}

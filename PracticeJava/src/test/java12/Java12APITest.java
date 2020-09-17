57e2d7d88ef560242aae1c1e6eb544cdef962914
sambunarayan <soyou30257@gmail.com>
2020-09-17 23:28:56 +0900

    20200917 Studying Java11,12API

---
+++ PracticeJava/src/test/java12/Java12APITest.java
@@ -0,0 +1,17 @@
+package test.java12;
+
+public class Java12APITest {
+	
+	enum day {
+		MONDAY,
+		TUESDAY,
+		WEDNESDAy,
+		THURSDAY,
+		FRIDAY
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}

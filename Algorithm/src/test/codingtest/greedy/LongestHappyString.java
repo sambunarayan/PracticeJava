259be0777e2e1b5c247c5c75a078d0fb96c8f405
sambunarayan <soyou30257@gmail.com>
2020-10-16 17:56:43 +0900

    20201016 java algorithm
    ・greedy

--- Algorithm/src/test/codingtest/greedy/LongestHappyString.java
+++ Algorithm/src/test/codingtest/greedy/LongestHappyString.java
@@ -24,64 +24,35 @@ public class LongestHappyString {
 	}
 	public String longestDiverseString(int a, int b, int c) {
 		StringBuilder s = new StringBuilder(a + b + c);
-		Pair aP = new Pair('a', a);
-		Pair bP = new Pair('b', b);
-		Pair cP = new Pair('c', c);
-		Queue<Pair> que = new PriorityQueue<>(new PairComparator());
-		if (a > 0) que.add(aP);
-		if (b > 0) que.add(bP);
-		if (c > 0) que.add(cP);
-			
-		while (que.size() > 1) {
-			int bef = 1;
-			int print = 0;
-			while (print < 2) {
-				Pair p = que.poll();
-				if (s.toString().endsWith(""+p.key)) {
-					if (!que.isEmpty()) p = que.poll();
-				}
-				int cnt = (p.val / 2 > 0) ? 2 : 1;
-				for (int i = 0; i < cnt && p.val > 0; i++) {
-					s.append(p.key);
-					p.val--;
-				}
-				bef = p.val;
-				print++;
+		int[] aa = {(int)'a', a};
+		int[] bb = {(int)'b', b};
+		int[] cc = {(int)'c', c};
+		Queue<int[]> que = new PriorityQueue<>((o1, o2)->{
+			return ((int[]) o2)[1] - ((int[]) o1)[1];
+		});
+		if (a > 0) que.add(aa);
+		if (b > 0) que.add(bb);
+		if (c > 0) que.add(cc);
+		
+		int[] bef = null;
+		while (!que.isEmpty()) {
+			int[] thisChar = que.poll();
+			String tmp = "" + (char)thisChar[0];
+			tmp += tmp;
+			if (s.length() >= 2 && s.toString().endsWith(tmp)) {
+				bef = thisChar;
+				continue;
 			}
-			if (aP.val > 0) que.add(aP);
-			if (bP.val > 0) que.add(bP);
-			if (cP.val > 0) que.add(cP);
-		}
-		if (!que.isEmpty()) {
-			Pair p = que.poll();
-			if (!s.toString().endsWith(""+p.key)) {
-				for (int i = 0; i < 2 && p.val > 0; i++) {
-					s.append(p.key);
-					p.val--;
-				}
+			s.append((char)thisChar[0]);
+			thisChar[1]--;
+			if (thisChar[1] > 0) que.add(thisChar);
+			if (bef != null) {
+				que.add(bef);
+				bef = null;
 			}
 		}
 		
         return s.toString();
     }
-	
-	class Pair {
-		char key;
-		Integer val;
-		
-		Pair(char key, Integer val) {
-			this.key = key;
-			this.val = val;
-		}
-	}
-	
-	class PairComparator implements Comparator<Pair> {
-
-		@Override
-		public int compare(Pair o1, Pair o2) {
-			return o2.val.compareTo(o1.val);
-		}
-		
-	}
 }
 

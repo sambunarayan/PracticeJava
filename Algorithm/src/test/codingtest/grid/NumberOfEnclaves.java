ad91dba49707a428d9068d8880193ed5a009188c
sambunarayan <soyou30257@gmail.com>
2021-03-17 22:27:26 +0900

    20210317 CodingTest
    ・Using DFS Algorithm

--- Algorithm/src/test/codingtest/grid/NumberOfEnclaves.java
+++ Algorithm/src/test/codingtest/grid/NumberOfEnclaves.java
@@ -3,7 +3,7 @@ package test.codingtest.grid;
 public class NumberOfEnclaves {
 	
 	// leetcode 1020.Number of Enclaves
-	// Not yet solved.
+	// Using DFS Algorithm
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,57 +12,38 @@ public class NumberOfEnclaves {
 	
 	public int numEnclaves(int[][] grid) {
         int res = 0;
-        for (int x = 0; x < grid.length; x++) {
-            for (int y = 0; y < grid[x].length; y++) {
-                if (grid[x][y] == 0 || grid[x][y] == 3) {
-                    continue;
-                } else if (grid[x][y] == 1) {
-                    walk(grid, x, y, false);
-                }
-                if (grid[x][y] != 3) {
-                    grid[x][y] = 4;
+        for (int y = 0; y < grid[0].length; y++) {
+            // first row and last row
+            walk(grid, 0, y);
+            walk(grid, grid.length - 1, y);
+        }
+        for (int x = 1; x < grid.length - 1; x++) {
+            // first column and last column
+            walk(grid, x, 0);
+            walk(grid, x, grid[x].length - 1);
+        }
+        // count 1
+        for (int x = 1; x < grid.length - 1; x++) {
+            for (int y = 1; y < grid[x].length - 1; y++) {
+                if (grid[x][y] == 1) {
                     res++;
                 }
             }
         }
-        // for (int x = 0; x < grid.length; x++) {
-        //     for (int y = 0; y < grid[x].length; y++) {
-        //         System.out.print(grid[x][y] + " ");
-        //     }
-        //     System.out.println();
-        // }
         return res;
     }
     
-    private boolean walk(int[][] grid, int x, int y, boolean canWalk) {
+    private void walk(int[][] grid, int x, int y) {
         if ((x < 0 || x >= grid.length)
-            || (y < 0 || y >= grid[x].length)) {
-            return true;
-        } else if (grid[x][y] == 0 || grid[x][y] == 2 || grid[x][y] == 4) {
-            return canWalk;
-        } else if (grid[x][y] == 3) {
-            return true;
+            || (y < 0 || y >= grid[x].length)){
+            return ;
+        } else if (grid[x][y] == 0 || grid[x][y] == 2) {
+            return ;
         }
-        if (!canWalk) {
-            grid[x][y] = 4;
-            boolean res = false;
-            res = walk(grid, x + 1, y, res);
-            res = walk(grid, x - 1, y, res);
-            res = walk(grid, x, y + 1, res);
-            res = walk(grid, x, y - 1, res);
-            if (res) {
-                grid[x][y] = 3;  
-            } else {
-                grid[x][y] = 4;    
-            }
-            return res;
-        } else {
-            grid[x][y] = 3;
-            walk(grid, x + 1, y, canWalk);
-            walk(grid, x - 1, y, canWalk);
-            walk(grid, x, y + 1, canWalk);
-            walk(grid, x, y - 1, canWalk);
-            return canWalk;
-        }
+        grid[x][y] = 2;
+        walk(grid, x + 1, y);
+        walk(grid, x - 1, y);
+        walk(grid, x, y + 1);
+        walk(grid, x, y - 1);
     }
 }

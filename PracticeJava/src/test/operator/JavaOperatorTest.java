59e0ec8bbdc69d14a7a5d324d54a04cd6dbaf9f2
sambunarayan <soyou30257@gmail.com>
2021-05-23 18:59:43 +0900

    20210523 Test Operator
    ・Shift Operator

--- PracticeJava/src/test/operator/JavaOperatorTest.java
+++ PracticeJava/src/test/operator/JavaOperatorTest.java
@@ -3,8 +3,13 @@ package test.operator;
 public class JavaOperatorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Shift Operator
+		// left shift
+		System.out.println("2 << 3 = " + (2 << 3));
+		// right shift
+		System.out.println("16 >> 3 = " + (16 >> 3));
+		System.out.println("-16 >> 3 = " + (-16 >> 3));
+		System.out.println("-16 >>> 3 = " + (-16 >>> 3));
 	}
 
 }
